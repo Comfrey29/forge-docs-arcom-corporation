04bc38dc7c1815ca4d3b1adb511e1990bae1a6c4
Comfrey29 <tiopepa37@gmail.com>
2025-11-01 09:47:58 +0100

    Update __init__.py

--- forgedocs/__init__.py
+++ forgedocs/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
+import os
 
 def create_app():
-    app = Flask(__name__)
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    template_dir = os.path.join(base_dir, '../templates')  # ajusta si cal segons la teva estructura
+
+    app = Flask(__name__, template_folder=template_dir)
     from .routes import main
     app.register_blueprint(main)
     return app
